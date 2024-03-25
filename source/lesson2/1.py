7465a81265ebe7c96568d04ca69c6cc52ced6a5b
raidua <benlyh@qq.com>
2024-03-25 20:16:34 +0800

    finished version

--- source/lesson2/1.py
+++ source/lesson2/1.py
@@ -1,1 +1,11 @@
-print("hello world")
+def F(n):
+    if n <= 1: return max(n,0)
+    f=0
+    f0=1
+    f1=1
+    for i in range(1, n-1):
+        f = f0 + f1
+        f0, f1 = f1, f
+    return f
+n=int(input("Please enter an integer."))
+print("Fibonacci number from 1 to", n, "is", F(n))
